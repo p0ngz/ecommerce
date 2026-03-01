975c6e7bb252070439b5387199477b487ea7f75b
p0ngz <naepongsatorn@gmail.com>
2026-03-01 08:12:42 +0700

    add toast in ProductTypePage

--- src/pages/ProductTypePage.jsx
+++ src/pages/ProductTypePage.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import TypeProductsComponent from "../components/TypeProductsComponent";
+import { ToastContainer } from "react-toastify";
 
 const ProductTypePage = () => {
-  return <TypeProductsComponent />;
+  return (
+    <>
+      <TypeProductsComponent />
+      <ToastContainer position="top-right" autoClose={2000} closeOnClick />
+    </>
+  );
 };
 
 export default ProductTypePage;
